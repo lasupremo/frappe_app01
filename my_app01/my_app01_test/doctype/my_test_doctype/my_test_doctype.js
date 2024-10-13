55e8013f8995e6a2b1cde3faac8eb0fce34f6853
lasupremo <71474991+lasupremo@users.noreply.github.com>
2024-10-13 19:32:58 +0000

    Create New DocType

---
+++ my_app01/my_app01_test/doctype/my_test_doctype/my_test_doctype.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Liam Supremo and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("My Test DocType", {
+// 	refresh(frm) {
+
+// 	},
+// });
